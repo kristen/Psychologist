ae96839383ecc8afb58f6e5baffccd1b42065dd8
Kristen Sundquist <kristen@csc8.com>
2012-07-14 17:12:55 -0700

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by Kristen Sundquist on 7/14/12.
+//  Copyright (c) 2012 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by Kristen Sundquist on 7/14/12.
+//  Copyright (c) 2012 Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
